5412c82e4561004de569ffb78ba8c62853408204
Scott Ladd <scottjr101@gmail.com>
2019-09-10 15:16:12 -0400

    cleaned up code and fix some lines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,5 @@
-//GLOBAL VARIABLES
-//---------------------------------------
 // Used to record how many times a letter can be pressed
-var doubleWord = ['a','b','c',
-				  'd','e','f',
-				  'g','h','i',
-				  'j','k','l',
-				  'm','n','o',
-				  'p','q','r',
-				  's','t','u',
-				  'v','w','x',
-				  'y','z'];
+var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 //Holds the all the words
 var wordBank =['chevrolet',
                'ford',
@@ -48,7 +38,6 @@ var loseCount = 0;
 var guessesLeft = 10;
 var rightGuessCounter = 0;
 //FUNCTIONS
-//----------------------------------------
 function reset()
 {
 	//Chooses word randombly from the wordBank
@@ -58,22 +47,13 @@ function reset()
 	//Get the number of blanks
 	numBlanks = lettersInWord.length;
 	
-	//RESET
-	//===========================================================
+	//Reset
 	letterGuessed = 0;
 	rightGuessCounter = 0;
 	guessesLeft = 10;
 	wrongLetters =[];
 	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
+	doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	test=false;
 	startGame();
 }
@@ -92,17 +72,9 @@ function startGame()
 	guessesLeft = 10;
 	wrongLetters =[];
 	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
+	doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-	//Populate blanks
+	//Creates blanks
 	for(var i = 0; i< numBlanks; i++)
 	{
 		blanksAndSuccesses.push('_');
@@ -118,7 +90,6 @@ function startGame()
 
 function compareLetters(userKey)
 {
-				console.log('WORKING!');
 				//If user key exist in choosen word then perform this function 
 				if(choosenWord.indexOf(userKey) > -1)
 				{
@@ -133,8 +104,7 @@ function compareLetters(userKey)
 							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
 						}	
 					}
-					//Test / Debug
-					console.log(blanksAndSuccesses);
+					
 				}
 				//Wrong Keys
 				else
@@ -144,9 +114,6 @@ function compareLetters(userKey)
 					//Changes HTML
 					document.getElementById('numGuesses').innerHTML = guessesLeft;
 					document.getElementById('wrongGuesses').innerHTML = wrongLetters;
-					//Test / Debug
-					console.log('Wrong Letters = ' + wrongLetters);
-					console.log('Guesses left are ' + guessesLeft);
 				}
 			
 			
@@ -161,7 +128,6 @@ function winLose()
 		winCount++;
 		//Changes HTML
 		document.getElementById('winCounter').innerHTML = winCount;
-		alert('You Win');
 		reset();
 	}
 	// When number of Guesses reaches 0 then You lose
@@ -169,14 +135,12 @@ function winLose()
 	{
 		//Counts losses
 		loseCount++;
-		//Alerts player they lose
-		alert('You Lose');
+		//Resets
 		reset();
 	}
 }
 
 //MAIN PROCCESS
-//-------------------------------------------	
 //Starts the Code
 startGame();
 
